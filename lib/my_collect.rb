181f3e3b8cc768243a74cff1f2c2d75398d1f66a
Agnes Park <agnespark824@gmail.com>
2020-01-24 04:54:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   second_array = []
   i = 0
   while i < array.size
-    second_array << my_collect (yield (array[i]))
+    second_array << yield (array[i])
     i += 1
   end
   second_array
